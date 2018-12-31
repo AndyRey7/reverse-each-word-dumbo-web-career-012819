38986f1ab2779d94913d6d8e84e59886e6f54b50
andy reyes <11710reyes@gmail.com>
2018-12-31 02:38:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   array = sentence.split(' ')
   new_array = []
-  sentence.each do |word|
+  array.each do |word|
     new_array.push(word.reverse)
   end
   return new_array.join(' ')
